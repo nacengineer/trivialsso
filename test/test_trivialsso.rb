5950e25a38a7a49f309da0c92edd97d0a99bb549
David Southard <nacengineer@gmail.com>
2013-02-19 10:28:42 -0600

    tabs --> spaces, fix mocha include

--- test/test_trivialsso.rb
+++ test/test_trivialsso.rb
@@ -2,7 +2,7 @@ require 'active_support/core_ext'
 require 'rails/all'
 
 require 'test/unit'
-require 'mocha'         # Need the mocha gem to properly stub out rails configuration.
+require 'mocha/setup'         # Need the mocha gem to properly stub out rails configuration.
 require 'trivialsso'
 
 
@@ -14,31 +14,31 @@ class TrivialssoTest < Test::Unit::TestCase
 	def setup
 		# Stub out our Rails config so we can test things properly.
 		Rails.stubs(:configuration).returns(Rails::Application::Configuration.allocate)
-		Rails.configuration.sso_secret = "57f236fdb162bd951f2ed15683a9d9d327f26ecdccb1897107161b76223b07a46d34907c3357e66b4eb0ef6e06888a700c0dc"			
+		Rails.configuration.sso_secret = "57f236fdb162bd951f2ed15683a9d9d327f26ecdccb1897107161b76223b07a46d34907c3357e66b4eb0ef6e06888a700c0dc"
 	end
 
 
-	def test_can_create_cookie		
+	def test_can_create_cookie
 		mycookie = Trivialsso::Login.cookie({'username' => 'testor'})
-		assert !mycookie.nil?		
+		assert !mycookie.nil?
 	end
 
 	def test_create_cookie_with_userdata
 		mycookie = Trivialsso::Login.cookie({'username' => 'testor', 'data' => 'additional cookie data'})
 		assert !mycookie.nil?
 	end
-	
+
 	def test_create_cookie_and_decode_it
 		mycookie = Trivialsso::Login.cookie({'username' => 'testor', 'data' => 'additional cookie data'})
 		data = Trivialsso::Login.decode_cookie(mycookie)
-		
+
 		if data['data'] == 'additional cookie data'
 			assert true
 		else
 			assert false
 		end
 	end
-	
+
 	def test_throw_exception_on_missing_username
 		begin
 			mycookie = Trivialsso::Login.cookie("")
@@ -48,42 +48,42 @@ class TrivialssoTest < Test::Unit::TestCase
 			assert false, "No exception was raised for a blank username"
 		end
 	end
-	
-	
+
+
 	def test_expire_date_exists
 		# in a full rails environment, this will return an ActiveSupport::TimeWithZone
 		mydate = Trivialsso::Login.expire_date
 		assert mydate.is_a?(Time), "proper Time object not returned"
 	end
-	
+
 	def test_expire_date_is_in_future
 		assert (DateTime.now < Trivialsso::Login.expire_date), "Expire date is in the past - cookie will expire immediatly."
 	end
-	
+
 	def test_exception_on_blank_cookie
 		begin
 			Trivialsso::Login.decode_cookie("")
 		rescue Trivialsso::MissingCookie
-			assert true			
+			assert true
 		else
 			assert false, "no exception was raised for a blank cookie"
 		end
 	end
-	
+
 	def test_exception_on_bad_cookie
 		begin
 			Trivialsso::Login.decode_cookie("BAhbB0kiC2RqbGVlMgY6BkVUbCsHo17iTg")
 		rescue Trivialsso::BadCookie
-			assert true			
+			assert true
 		else
 			assert false, "no exception was raised for a malformed cookie"
-		end		
+		end
 	end
-	
+
 	def test_exception_on_expired_cookie
 		#create a cookie that is expired.
 		temp_cookie = Trivialsso::Login.cookie({'username' => 'testor'}, 2.seconds.ago)
-		
+
 		begin
 			Trivialsso::Login.decode_cookie(temp_cookie)
 		rescue Trivialsso::LoginExpired
@@ -92,5 +92,5 @@ class TrivialssoTest < Test::Unit::TestCase
 			assert false, "no exception was raised for an expired cookie"
 		end
 	end
-	
+
 end
