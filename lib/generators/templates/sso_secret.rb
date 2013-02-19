94bbd560753b311704362622e11897b42e422a5d
David Southard <nacengineer@gmail.com>
2013-02-19 16:23:13 -0600

    use SecureRandom

--- lib/generators/templates/sso_secret.rb
+++ lib/generators/templates/sso_secret.rb
@@ -1,4 +1,5 @@
 # This file is required to define the shared secret used for generating and decoding
 # SSO cookies. This secret *must* be the same across all of your applications.
+require 'securerandom'
 
-<%= Rails.application.class.parent_name %>::Application.config.sso_secret = "You-really-need-to-change-this-to-a-nice-long-random-string"
+<%= Rails.application.class.parent_name %>::Application.config.sso_secret = SecureRandom.hex(64)
